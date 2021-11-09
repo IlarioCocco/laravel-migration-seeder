e6b93844b37ca2a3c6da8ab9cec95d9b1f804cf7
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-09 17:22:58 +0100

    first commit

---
+++ database/migrations/2021_11_09_145859_create_travels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string("code_travel", 20)->unique();
+            $table->string("destination", 50);
+            $table->string("means_of_transport", 20);
+            $table->date("departure_date");
+            $table->date("returne_date");
+            $table->tinyInteger("number_passenger");
+            $table->string("guide", 20);
+            $table->float("price", 7, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
