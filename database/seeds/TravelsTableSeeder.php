1e32aafa64427538ac960c8e2d8b150ce26fa152
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-10 00:03:11 +0100

    add librery faker

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Travel;
+use Faker\Generator as Faker;
 
 class TravelsTableSeeder extends Seeder
 {
@@ -10,17 +11,30 @@ class TravelsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $newTravel = new Travel();
-        $newTravel->code_travel = "abc-123";
-        $newTravel->destination = "italy";
-        $newTravel->means_of_transport = "aereo";
-        $newTravel->departure_date = 21/02;
-        $newTravel->returne_date = 25/02;
-        $newTravel->number_passenger = 2;
-        $newTravel->guide = "locale";
-        $newTravel->price = 1000.00;
-        $newTravel->save();
+        for ( $i = 0; $i < 100; $i++ ) {
+
+            $newTravel = new Travel();
+            $newTravel->code_travel = $faker->ean8();
+            $newTravel->destination = $faker->state();
+            $newTravel->means_of_transport = $faker->company();
+            $newTravel->departure_date = $faker->dateTime();
+            $newTravel->returne_date = $faker->dateTime();
+            $newTravel->number_passenger = $faker->randomDigit(1,9);
+            $newTravel->guide = $faker->text();
+            $newTravel->price = $faker->randomFloat(2,1000,100000);
+            $newTravel->save();
+        }
+        // $newTravel = new Travel();
+        // $newTravel->code_travel = "abc-125";
+        // $newTravel->destination = "italy";
+        // $newTravel->means_of_transport = "aereo";
+        // $newTravel->departure_date = 21/02;
+        // $newTravel->returne_date = 25/02;
+        // $newTravel->number_passenger = 2;
+        // $newTravel->guide = "locale";
+        // $newTravel->price = 1000.00;
+        // $newTravel->save();
     }
 }
